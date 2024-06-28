a625ae0182e69165ecbe34d668f1d89c28856896
vickiwu <794431483@qq.com>
2024-06-28 15:43:39 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      message: ''
+    }
+  },
+  mounted() {
+    const result = window.myLibrary.add(5, 3);
+    this.message = `Result of add(5, 3): ${result}`;
   }
 }
 </script>
---
+++ src/preload.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: vickiwu 794431483@qq.com
+ * @Date: 2024-06-28 14:56:07
+ * @LastEditors: vickiwu 794431483@qq.com
+ * @LastEditTime: 2024-06-28 15:26:42
+ * @FilePath: /electron-vue-app/src/preload.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// preload.js
+
+// 所有的 Node.js API接口 都可以在 preload 进程中被调用.
+// 它拥有与Chrome扩展一样的沙盒。
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
